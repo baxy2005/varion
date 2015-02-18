de53fca7d89ebb8677187a1ef991fba98836e18c
andras varadi <andris@varadi.tv>
2015-02-18 22:09:01 +0100

    getting started the projekt, adding navigation and the front image

--- components/js/script.js
+++ components/js/script.js
@@ -1,5 +1,36 @@
 // JavaScript Document
-function test() {
-    return "hi ";
-	
-}
+  var wheight = $(window).height(); //get height of the window
+
+  $('.fullheight').css('height', wheight);
+
+  $(window).resize(function() {
+    var wheight = $(window).height(); //get height of the window
+    $('.fullheight').css('height', wheight);
+  })
+  
+// Animated Scrolling
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset
+        }, 1000);
+        return false;
+      } // target.length
+    } //location hostname
+  }); //on click
+
+
+  //set up ScrollMagic
+  var controller = new ScrollMagic({
+    globalSceneOptions: {
+      triggerHook: "onLeave"
+    }
+  });
+
+  //pin the navigation
+  var pin = new ScrollScene({
+    triggerElement: '#nav',
+  }).setPin('#nav').addTo(controller);
