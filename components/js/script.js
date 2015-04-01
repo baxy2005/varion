7ff73090a92b198161d505e0889db7b4703a0885
Varadi Andris <andris@varadi.tv>
2015-04-01 09:16:26 +0200

    fancy box gallery and fade in stages and loading

--- components/js/script.js
+++ components/js/script.js
@@ -1,4 +1,83 @@
 // JavaScript Document
+
+$(window).load(function(){
+	"use strict";
+$("#loader").fadeOut("slow");
+	var speedOpacity = 2000;
+	$('body').animate({
+		opacity: 1
+	}, speedOpacity, function() {});
+});
+
+$(document).ready(function(){
+	"use strict";
+
+	(function($) {
+
+		$.fn.visible = function(partial) {
+
+			var $t            = $(this),
+				$w            = $(window),
+				viewTop       = $w.scrollTop(),
+				viewBottom    = viewTop + $w.height(),
+				_top          = $t.offset().top,
+				_bottom       = _top + $t.height(),
+				compareTop    = partial === true ? _bottom : _top,
+				compareBottom = partial === true ? _top : _bottom;
+
+			return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+		};
+
+	})(jQuery);
+
+	var win = $(window);
+
+	var setVisibleBlock = function() {
+		var allMods = $("#welcome, #link2, #link3, #link4, #link5, #link6, #link7, #link8, .whyBgImage, .bg-lightGray");
+		allMods.each(function(i, el) {
+			var $el = $(el);
+			if ($el.visible(true)) {
+				$el.addClass("fade-in");
+			}
+		});
+	};
+
+	setVisibleBlock();
+
+	/* --------- Options Rate-paralax ------------ */
+
+	var opacityRate = 1.2;
+	var speedRate = 4;
+	var backgroundSpeedRate = 1;
+
+
+
+	var header = $('.header');
+	var promoEffect = $('.container.promo-effect');
+	var promo = $('.promo');
+
+	var lastScrollTop = 0;
+
+	win.scroll(function() {
+		if (header.length) {
+		   promoEffect.css('opacity', (1 - $(document).scrollTop() * opacityRate / header.offset().top));
+		}
+		promoEffect.css('top', (-1) * $(document).scrollTop() / speedRate);
+		promo.css('background-position', "0px " + $(document).scrollTop() * backgroundSpeedRate + "px");
+
+		var scrollTop = $(this).scrollTop();
+
+		if (scrollTop > lastScrollTop) {
+			setVisibleBlock();
+		}
+
+		lastScrollTop = scrollTop;
+
+	});
+
+});
+
 var topoffset = 90;
   var isTouch = 'ontouchstart' in document.documentElement;
 
@@ -54,4 +133,5 @@ var topoffset = 90;
   var pin = new ScrollScene({
     triggerElement: '#nav',
   }).setPin('#nav').addTo(controller);
+  
   
