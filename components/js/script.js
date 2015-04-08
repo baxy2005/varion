4ff695c45df06a4c6d002d573e54d1aea030e90f
Varadi Andris <andris@varadi.tv>
2015-04-08 14:03:11 +0200

    owl test page

--- components/js/script.js
+++ components/js/script.js
@@ -7,76 +7,89 @@ $("#loader").fadeOut("slow");
 	$('body').animate({
 		opacity: 1
 	}, speedOpacity, function() {});
+	
+	  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 290,
+    itemMargin: 1,
+    minItems: 1,
+    maxItems: 4
+  });
+
+
 });
 
-$(document).ready(function(){
-	"use strict";
-
-	(function($) {
-
-		$.fn.visible = function(partial) {
-
-			var $t            = $(this),
-				$w            = $(window),
-				viewTop       = $w.scrollTop(),
-				viewBottom    = viewTop + $w.height(),
-				_top          = $t.offset().top,
-				_bottom       = _top + $t.height(),
-				compareTop    = partial === true ? _bottom : _top,
-				compareBottom = partial === true ? _top : _bottom;
-
-			return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-
+	$(document).ready(function(){
+		"use strict";
+	
+		(function($) {
+	
+	
+			$.fn.visible = function(partial) {
+	
+				var $t            = $(this),
+					$w            = $(window),
+					viewTop       = $w.scrollTop(),
+					viewBottom    = viewTop + $w.height(),
+					_top          = $t.offset().top,
+					_bottom       = _top + $t.height(),
+					compareTop    = partial === true ? _bottom : _top,
+					compareBottom = partial === true ? _top : _bottom;
+	
+				return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+	
+			};
+	
+		})(jQuery);
+		
+		var win = $(window);
+	
+		var setVisibleBlock = function() {
+			var allMods = $("#welcome, #link2, #link3, #link4, #link5, #link6, #link7, #link8, .whyBgImage, .bg-lightGray");
+			allMods.each(function(i, el) {
+				var $el = $(el);
+				if ($el.visible(true)) {
+					$el.addClass("fade-in");
+				}
+			});
 		};
-
-	})(jQuery);
-
-	var win = $(window);
-
-	var setVisibleBlock = function() {
-		var allMods = $("#welcome, #link2, #link3, #link4, #link5, #link6, #link7, #link8, .whyBgImage, .bg-lightGray");
-		allMods.each(function(i, el) {
-			var $el = $(el);
-			if ($el.visible(true)) {
-				$el.addClass("fade-in");
+	
+		setVisibleBlock();
+		
+	
+		/* --------- Options Rate-paralax ------------ */
+	
+		var opacityRate = 1.2;
+		var speedRate = 4;
+		var backgroundSpeedRate = 1;
+	
+	
+		var header = $('.header');
+		var promoEffect = $('.container.promo-effect');
+		var promo = $('.promo');
+	
+		var lastScrollTop = 0;
+	
+		win.scroll(function() {
+			if (header.length) {
+			   promoEffect.css('opacity', (1 - $(document).scrollTop() * opacityRate / header.offset().top));
 			}
+			promoEffect.css('top', (-1) * $(document).scrollTop() / speedRate);
+			promo.css('background-position', "0px " + $(document).scrollTop() * backgroundSpeedRate + "px");
+	
+			var scrollTop = $(this).scrollTop();
+	
+			if (scrollTop > lastScrollTop) {
+				setVisibleBlock();
+			}
+	
+			lastScrollTop = scrollTop;
+	
 		});
-	};
-
-	setVisibleBlock();
-
-	/* --------- Options Rate-paralax ------------ */
-
-	var opacityRate = 1.2;
-	var speedRate = 4;
-	var backgroundSpeedRate = 1;
-
-
-
-	var header = $('.header');
-	var promoEffect = $('.container.promo-effect');
-	var promo = $('.promo');
-
-	var lastScrollTop = 0;
-
-	win.scroll(function() {
-		if (header.length) {
-		   promoEffect.css('opacity', (1 - $(document).scrollTop() * opacityRate / header.offset().top));
-		}
-		promoEffect.css('top', (-1) * $(document).scrollTop() / speedRate);
-		promo.css('background-position', "0px " + $(document).scrollTop() * backgroundSpeedRate + "px");
-
-		var scrollTop = $(this).scrollTop();
-
-		if (scrollTop > lastScrollTop) {
-			setVisibleBlock();
-		}
-
-		lastScrollTop = scrollTop;
-
+		
+	
 	});
-
-});
 
 var topoffset = 90;
   var isTouch = 'ontouchstart' in document.documentElement;
@@ -135,3 +148,5 @@ var topoffset = 90;
   }).setPin('#nav').addTo(controller);
   
   
+
+
